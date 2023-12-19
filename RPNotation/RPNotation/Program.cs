625ef04d9c815a2f44bb7f987168707d1f482a96
dhorbon <szymon080206@gmail.com>
2023-12-19 12:55:49 +0100

    initial

---
+++ RPNotation/RPNotation/Program.cs
@@ -0,0 +1,53 @@
+﻿for(; ; )
+{
+    Console.Write(
+        "Supported operations:\n" +
+        " Whole Numbers\n" +
+        " + (addition), - (subtraction), * (multiplication), / (whole dicision), ^ (exponentation) operations\n" +
+        "Enter a string separating operands with spaces:\n"
+        );
+    string[] input = Console.ReadLine().Split(' ');
+
+    List<int> stack = new List<int>();
+    for(int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == "+")
+        {
+            stack[stack.Count - 2] = stack[stack.Count - 2] + stack[stack.Count -1];
+            stack.RemoveAt(stack.Count - 1);
+        }
+        else if (input[i] == "-")
+        {
+            stack[stack.Count - 2] = stack[stack.Count - 2] - stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+        }
+        else if (input[i] == "*")
+        {
+            stack[stack.Count - 2] = stack[stack.Count - 2] * stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+        }
+        else if (input[i] == "/")
+        {
+            stack[stack.Count - 2] = stack[stack.Count - 2] / stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+        }
+        else if (input[i] == "^")
+        {
+            stack[stack.Count - 2] = stack[stack.Count - 2] ^ stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+        }
+        else
+        {
+            stack.Add(Convert.ToInt32(input[i]));
+        }
+    }
+
+    if(stack.Count != 1)
+    {
+        Console.WriteLine("wrong number of operands");
+    }
+    else
+    {
+        Console.WriteLine(stack[0]);
+    }
+}
